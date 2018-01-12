fc5ae95a5dd943a6619f5cea4efe9dfb3c91bffd
Martin Hjelmare <marhje52@kth.se>
2018-01-12 14:24:02 +0100

    Fix bug in setup_all_modules
    * Use regular import_module function to import module.

--- camacq/helper.py
+++ camacq/helper.py
@@ -95,14 +95,14 @@ def setup_all_modules(center, config, package_path, **kwargs):
     """
     imported_pkg = import_module(package_path)
     # yields, non recursively, modules under package_path
-    for loader, name, is_pkg in pkgutil.iter_modules(
+    for _, name, is_pkg in pkgutil.iter_modules(
             imported_pkg.__path__, prefix='{}.'.format(imported_pkg.__name__)):
         if 'main' in name:
             continue
         if name in _MODULE_CACHE:
             module = _MODULE_CACHE[name]
         else:
-            module = loader.find_module(name).load_module(name)
+            module = import_module(name)
             _MODULE_CACHE[name] = module
         _LOGGER.debug('Loaded %s', name)
         keys = [
@@ -111,10 +111,10 @@ def setup_all_modules(center, config, package_path, **kwargs):
         pkg_config = _deep_conf_access(config, keys)
         if module.__name__.split('.')[-1] in pkg_config:
             if is_pkg and hasattr(module, 'setup_package'):
-                _LOGGER.info('Setting up %s', module.__name__)
+                _LOGGER.info('Setting up %s package', module.__name__)
                 module.setup_package(center, config, **kwargs)
             elif hasattr(module, 'setup_module'):
-                _LOGGER.info('Setting up %s', module.__name__)
+                _LOGGER.info('Setting up %s module', module.__name__)
                 module.setup_module(center, config, **kwargs)
 
 
