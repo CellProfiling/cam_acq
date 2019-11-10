c7ed4e5f22cb85c5d3b5662bcd5046e43326c327
Martin Hjelmare <marhje52@kth.se>
2019-11-10 03:11:10 +0100

    Enhance actions interface
    * Allow calling actions via dot notation.

--- camacq/util.py
+++ camacq/util.py
@@ -19,6 +19,14 @@ except AttributeError:
             loop.close()
 
 
+class dotdict(dict):  # pylint: disable=invalid-name
+    """Access to dictionary attributes with dot notation."""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
 def read_csv(path, index=None):
     """Read a csv file and return a dict of dicts.
 
