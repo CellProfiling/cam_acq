dd60cf7df04c38df3d4decd2ff7ccc02a8d7ee73
Martin Hjelmare <marhje52@kth.se>
2019-12-25 23:19:28 +0100

    Clean up const

--- camacq/const.py
+++ camacq/const.py
@@ -4,21 +4,10 @@ CONF_DATA = "data"
 CONF_ID = "id"
 CONF_TRIGGER = "trigger"
 CONFIG_DIR = "config_dir"
-JOB_ID = "--E{:02d}"
-WELL_U_ID = "--U{:02d}"
-WELL_V_ID = "--V{:02d}"
-FIELD_X_ID = "--X{:02d}"
-FIELD_Y_ID = "--Y{:02d}"
-CHANNEL_ID = "--C{:02d}"
-WELL_NAME = (WELL_U_ID + WELL_V_ID)[2:]
-FIELD_NAME = (FIELD_X_ID + FIELD_Y_ID)[2:]
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_IMAGING_DIR = "imaging_dir"
 LOG_LEVEL = "log_level"
-WELL = "well"
-CONF_PLUGINS = "plugins"
-PACKAGE = "camacq"
 
 # events
 BASE_EVENT = "base_event"
