116cbbe6f787f668c96626a73e91efae8434b277
Martin Hjelmare <marhje52@kth.se>
2020-01-07 15:55:03 +0100

    Fix config test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,26 +1,10 @@
 """Test config."""
-import os
-import tempfile
-
-import pytest
-
 import camacq.config as config_util
 
-# pylint: disable=redefined-outer-name
 
-
-@pytest.fixture
-def config_dir():
-    """Return a path to temporary dir."""
-    temp_dir = tempfile.gettempdir()
-    yield temp_dir
-    config_path = os.path.join(temp_dir, "config.yml")
-    if not os.path.isfile(config_path):
-        return
-    os.remove(config_path)
-
-
-def test_ensure_config(config_dir):
+def test_ensure_config(tmp_path):
     """Test that a default config can be created."""
+    config_dir = tmp_path / "config_dir"
+    config_dir.mkdir()
     config_path = config_util.ensure_config_exists(config_dir)
-    assert config_path == os.path.join(config_dir, "config.yml")
+    assert config_path == config_dir / "config.yml"
