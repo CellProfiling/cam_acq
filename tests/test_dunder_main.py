3c37d91bcd22dae637825b48011e60b76858d412
Martin Hjelmare <marhje52@gmail.com>
2026-03-07 04:42:06 +0100

    chore: shorten cli assertion

--- tests/test_dunder_main.py
+++ tests/test_dunder_main.py
@@ -12,4 +12,4 @@ def test_can_run_as_python_module() -> None:
         capture_output=True,
     )
     assert result.returncode == 0
-    assert b"usage: __main__.py [-h]" in result.stdout
+    assert b"usage:" in result.stdout
