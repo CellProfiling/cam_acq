e69398864e53eeaa922521a572dbf78fd1845aa9
Martin Hjelmare <marhje52@gmail.com>
2026-03-27 22:34:59 +0100

    chore: fix linting

--- tests/test_dunder_main.py
+++ tests/test_dunder_main.py
@@ -6,7 +6,7 @@ import sys
 
 def test_can_run_as_python_module() -> None:
     """Run the CLI as a Python module."""
-    result = subprocess.run(  # noqa: S603
+    result = subprocess.run(
         [sys.executable, "-m", "camacq", "--help"],
         check=True,
         capture_output=True,
